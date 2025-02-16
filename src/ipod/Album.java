a6757df4c8ce24268b1738554117bfc3c1dd48f7
FrancineDelanni02 <delannifrancine@gmail.com>
2025-02-16 02:39:39 -0300

    update Album.java

--- src/ipod/Album.java
+++ src/ipod/Album.java
@@ -1,9 +1,11 @@
 package ipod;
 
 import java.util.Date;
+import java.util.List;
 
 public class Album {
     private String nome;
     private Date dataLancamento;
     private String descricao;
+    private List<Musica> musicas;
 }
